32f44c5f2fc09503394dcfd31d2ba226ce516f68
Pranav Patel <pranav@qualpay.com>
2021-05-03 09:23:29 -0700

    no message

--- src/com/pranav/educative/A_slidingwindow/A1_avg_of_all_contiguos_subarray.java
+++ src/com/pranav/educative/A_slidingwindow/A1_avg_of_all_contiguos_subarray.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 /**
  * @author pranavpatel
+ * 
+ * https://www.youtube.com/watch?v=KtpqeN0Goro&list=PL_z_8CaSLPWeM8BDJmIYDaoQ5zuwyxnfj&index=3
  *
  */
 
@@ -18,15 +20,20 @@ import java.util.List;
 // more like a 2 - pointer approach
 public class A1_avg_of_all_contiguos_subarray {
 
-  // ask if it is ok to retuen list
+  // brut force approach
   public static double[] findAvgIterative(int[] nums, int k) {
 
+    
+    // for array size 7 window size 3 prints array of size 5 that is why 7-3+1
+    // for array size 7 window size 4 prints array of size 4 that is why 7-4+1
+    
+    
     double[] res = new double[nums.length - k + 1];
 
     // left pointer will max go to nums.length-k
     for (int left = 0; left <= nums.length - k; left++) {
       double sum = 0;
-      // right starts with left always and goes to max to k at a time 
+      // right starts with left always and goes to max to k at a time
       for (int right = left; right < left + k; right++) {
         sum = sum + nums[right];
       }
@@ -40,9 +47,9 @@ public class A1_avg_of_all_contiguos_subarray {
   // space O(N) - if we consider o/p array
   public static double[] findAvgSlidingWindow(int[] nums, int k) {
 
+    // for array size 7 window size 3 prints array of size 5 that is why 7-3+1
+    // for array size 7 window size 4 prints array of size 4 that is why 7-4+1
     
-    // just take an example of 7 length array , it will be 3 elements and then make
-    // this pattern
     double[] res = new double[nums.length - k + 1];
 
     double window_sum = 0;
@@ -51,11 +58,14 @@ public class A1_avg_of_all_contiguos_subarray {
 
       window_sum = window_sum + nums[window_end]; // add the next element
 
-      // slide the window, we don't need to slide if we've not hit the required window
-      // size of 'k'
-      // so for first 4 index, it will not go here and then it will start adding each
-      // element and removing it
-      if (window_end >= k - 1) {
+      // how we decide window size window-end - window_start + 1
+      //https://www.youtube.com/watch?v=KtpqeN0Goro&list=PL_z_8CaSLPWeM8BDJmIYDaoQ5zuwyxnfj&index=3
+      
+      
+       /* we don't need to slide if we've not hit the required window size of 'k'
+       so for first 4 index, it will not go here and then it will start adding each
+       element and removing it*/
+      if (window_end-window_start+1 == k) {
 
         res[window_start] = window_sum / k; // window_avg
         window_sum = window_sum - nums[window_start]; // remove element going out of window
@@ -69,13 +79,14 @@ public class A1_avg_of_all_contiguos_subarray {
 
   public static void main(String[] args) {
 
-//    double[] res = findAvgIterative(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
-//
-//    double[] result1 = findAvgSlidingWindow(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
-//
-//    System.out.println(Arrays.toString(res));
-//    System.out.println(Arrays.toString(result1));
-    
+    double[] res = findAvgIterative(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
+
+    double[] result1 = findAvgSlidingWindow(new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 }, 5);
+  
+
+    System.out.println(Arrays.toString(res));
+    System.out.println(Arrays.toString(result1));
+
     System.out.println("p12passphrase".toCharArray());
 
   }
